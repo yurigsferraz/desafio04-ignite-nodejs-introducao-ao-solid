93500690890a20226d0d73904ca620d93c1df437
Yuri Ferraz <yurigsferraz@gmail.com>
2022-12-04 02:12:27 -0300

    Permite administrador listar todos os usuarios

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -3,11 +3,15 @@ import { Request, Response } from "express";
 import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
 
 class ListAllUsersController {
-  constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
+    constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.body;
+
+        this.listAllUsersUseCase.execute({ user_id });
+
+        return response.status(200).send();
+    }
 }
 
 export { ListAllUsersController };
