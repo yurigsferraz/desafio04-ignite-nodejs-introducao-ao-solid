a22bfb0aa00a0ec2126b9a00255a5b586038133c
Yuri Ferraz <yurigsferraz@gmail.com>
2022-12-04 01:47:29 -0300

    Permite tornar o usuário administrador

--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -3,11 +3,15 @@ import { Request, Response } from "express";
 import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
 
 class TurnUserAdminController {
-  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+    constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.body;
+
+        this.turnUserAdminUseCase.execute({ user_id });
+
+        return response.status(200).send();
+    }
 }
 
 export { TurnUserAdminController };
