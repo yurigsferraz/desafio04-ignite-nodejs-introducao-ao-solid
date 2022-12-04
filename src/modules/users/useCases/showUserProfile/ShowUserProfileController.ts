4814eaed008bdeba3549712f5882038801b7e816
Yuri Ferraz <yurigsferraz@gmail.com>
2022-12-04 02:20:27 -0300

    Permite exibir usuário por id

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,11 +3,15 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+    constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { user_id } = request.body;
+
+        this.showUserProfileUseCase.execute({ user_id });
+
+        return response.status(200).send();
+    }
 }
 
 export { ShowUserProfileController };
