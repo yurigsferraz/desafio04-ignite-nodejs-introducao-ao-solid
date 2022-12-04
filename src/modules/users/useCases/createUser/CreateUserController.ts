f24c3626680956be5db043ea498502f11dd63019
Yuri Ferraz <yurigsferraz@gmail.com>
2022-12-04 01:02:50 -0300

    Conserta cenários de teste
    UserRepository e CreateUseCase

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -3,11 +3,15 @@ import { Response, Request } from "express";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 class CreateUserController {
-  constructor(private createUserUseCase: CreateUserUseCase) {}
+    constructor(private createUserUseCase: CreateUserUseCase) {}
 
-  handle(request: Request, response: Response): Response {
-    // Complete aqui
-  }
+    handle(request: Request, response: Response): Response {
+        const { email, name } = request.body;
+
+        this.createUserUseCase.execute({ email, name });
+
+        return response.status(201).send();
+    }
 }
 
 export { CreateUserController };
--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -2,16 +2,24 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  name: string;
-  email: string;
+    name: string;
+    email: string;
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ email, name }: IRequest): User {
-    // Complete aqui
-  }
+    execute({ email, name }: IRequest): User {
+        const userWithEmailAlreadyUsed = this.usersRepository.findByEmail(
+            email
+        );
+
+        if (userWithEmailAlreadyUsed) {
+            throw new Error("Email is already being used!");
+        }
+
+        return this.usersRepository.create({ email, name });
+    }
 }
 
 export { CreateUserUseCase };
